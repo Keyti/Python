e716a0a63a674ec74704c5c599461a845dbe6e66
Kate <ignis.frozen@gmail.com>
2018-01-08 21:23:09 +0100

    Update NameGenerator.py

--- NameGenerator.py
+++ NameGenerator.py
@@ -1,4 +1,5 @@
 import random
+import os
 
 kobiety = ["Ala", "Ela", "Joasia", "Genowefa"]
 kobietyNazwiska = ["Jagoda", "Kowalska", "Nowak", "Hesz"]
@@ -7,27 +8,24 @@ mezczyzniNazwiska = ["Hucz", "Jurkowski", "Pupiec", "Oleander"]
 
 
 def imienieinazwisko(plec):
-    if plec == "k":
-        imie = random.choice(kobiety)
+    if plec == "w":
+        imie = random.choice(kobiety) + " " + random.choice(kobietyNazwiska)
+        print("Generated name is: " + imie)
     elif plec == "m":
-        imie = random.choice(mezczyzni)
+        imie = random.choice(mezczyzni) + " " + random.choice(mezczyzniNazwiska)
+        print("Generated name is: " + imie)
     else:
-        plec = input("Wpisz k dla kobiety i m dla mężczyzny")
-    if plec == "k":
-        nazwisko = random.choice(kobietyNazwiska)
-    elif plec == "m":
-        nazwisko = random.choice(mezczyzniNazwiska)
-    else:
-        plec = input("Wpisz k dla kobiety i m dla mężczyzny")
+      print("Wrong letter :( Only 'w' or 'm' ")
 
-    print("Wygenerowano imię: " + imie +" " + nazwisko)
 
-gender = input("Jakiej płci ma być wygenerowane imię i nazwisko? Wpisz k dla kobiety i m dla mężczyzny\n")
+
+gender = input("What gender should be the name we will generate? Choose w for woman and m for men\n")
 imienieinazwisko(gender)
-czy = input("Czy chcesz generować jeszcze raz? (y)es lub cokolwiek ;) \n")
+czy = input("Generate again? Choose y for yes or type anything else for exit ;) \n")
 
 while czy == "y":
-    gender = input("Jakiej płci ma być wygenerowane imię i nazwisko? Wpisz k dla kobiety i m dla mężczyzny\n")
+    os.system('cls')
+    gender = input("What gender should be the name we will generate? Choose w for woman and m for men\n")
     imienieinazwisko(gender)
-    czy = input("Czy chcesz generować jeszcze raz? (y)es lub cokolwiek ;) \n")
-print("No to cześć ;)")
+    czy = input("Generate again? Choose y for yes or type anything else for exit \n")
+print("Ok. Bye ;)")
