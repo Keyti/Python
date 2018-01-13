1cc0e79a5dba6cf4ee7fcdd02bbc47d592052dba
Kate <ignis.frozen@gmail.com>
2018-01-13 16:36:20 +0100

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -7,6 +7,7 @@ def FizzBuzz():
     start = int(input("Give me the start number:  "))
     stop = int(input("And the finish one:  "))
     lista = []
+    lista2 = []
 
     for i in range(start, stop + 1, 1):
         lista.append(i)
@@ -19,13 +20,17 @@ def FizzBuzz():
         for n in lista:
             if n % 3 == 0 and (n % 5 != 0):
                 print(n," Fizz")
+                lista2.append("Fizz")
             elif n % 5 == 0 and (n % 3 != 0):
                 print(n," Buzz")
+                lista2.append("Buzz")
             elif (n % 5 == 0) and (n % 3 == 0):
                 print(n," FizzBuzz")
+                lista2.append("FizzBuzz")
             else:
                 print(n)
-
+                lista2.append(n)
+    print(lista2)
 
 print("It's the FizzBuzz game! We will change all numbers divided by 5 with Buzz and all divided by 3 with Fizz")
 FizzBuzz()
